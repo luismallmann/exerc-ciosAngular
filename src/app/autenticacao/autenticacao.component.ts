4a18595004a748bd3aede777b83a8429c3158ace
luismallmann <luis.mallmann@gmail.com>
2022-08-22 00:16:35 -0300

    desenvolvimento lista 4

--- src/app/autenticacao/autenticacao.component.ts
+++ src/app/autenticacao/autenticacao.component.ts
@@ -1,44 +1,54 @@
 import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Usuario} from 'src/app/interfaces/usuario';
 
 @Component({
   selector: 'app-autenticacao',
   templateUrl: './autenticacao.component.html',
   styleUrls: ['./autenticacao.component.css']
 })
-export class AutenticacaoComponent{
-  email:string="";
-  password:string="";
-  msn:string="";
-  cor: string="";
+export class AutenticacaoComponent {
+  user: string = "";
+  password: string = "";
+  msn?: string; 
   qntErros = 0;
 
+  usuario: Usuario ={
+    userId:"",
+    password:"",
+    tipo:""
+  };
+  //inicializa a cor
+  cor = [""];
+
   //Função responsável por validar o login
-  login(){
+  login() {
+    this.user=this.usuario.userId;
+    this.password = this.usuario.password;
 
     //Caso login e senha correta
-    if(this.email == "XPTO-21" && this.password == "Trocar@123"){
-      this.msn="Logado!";
-      this.cor="green";
+    if (this.user == "XPTO-21" && this.password == "Trocar@123") {
+      this.msn = "Logado!";
+      this.cor = ["sucesso"]
     }
     //Se não estiver correta
-    else{
+    else {
       //Analisa o usuario
-      if(this.email != "XPTO-21"){
-        this.msn="Acesso negado, usuário incorreto";
-        this.cor="red";
+      if (this.user != "XPTO-21") {
+        this.msn = "Acesso negado, usuário incorreto";
+        this.cor = ["erro"]
       }
       //Analisa a senha
-      else if(this.password != "Trocar@123"){
-        this.msn="Acesso negado, senha incorreta";
+      else if (this.password != "Trocar@123") {
+        this.msn = "Acesso negado, senha incorreta";
         this.qntErros++;
-        this.cor="red";
+        this.cor = ["erro"]
       }
     }
     //Caso exceda três tentativas, o usuário é bloqueado
-    if(this.qntErros >=3){
-      this.msn ="Usuário Bloqueado!";
-      this.cor="red";
-    }  
+    if (this.qntErros >= 3) {
+      this.msn = "Usuário Bloqueado!";
+      this.cor = ["erro"]
+    }
   }
 }
