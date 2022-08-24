ba8db29c5f4997245116e4821f8b7dbaea597c57
luismallmann <luis.mallmann@gmail.com>
2022-08-23 22:17:09 -0300

    Inicio do desenvolvimento da lista de exercícios 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AutenticacaoComponent } from './autenticacao/autenticacao.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './pages/home/home.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { Error404Component } from './pages/error404/error404.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AutenticacaoComponent,
+    HomeComponent,
+    ContatoComponent,
+    Error404Component,
   ],
   imports: [
     BrowserModule,
